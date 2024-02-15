0a0aa8451f011c733e37c6ad049241815bee93eb
Karlo Marosini <karlo.marosini@cloudsense.com>
2024-02-15 10:39:07 +0100

    IDapper installed and created

---
+++ PartnerWeb/DataAccess/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PartnerWeb.DataAccess
+{
+    public class DapperContext : IDapperContext
+    {
+        private readonly IConfiguration _iConfiguration;
+        private readonly string _conn;
+
+        public DapperContext(IConfiguration iConfiguration)
+        {
+            _iConfiguration = iConfiguration;
+            _conn = _iConfiguration.GetConnectionString("InsuranceDatabaseConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_conn);
+
+    }
+}
